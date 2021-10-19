f41a041eff9a3a49a8268d3b730c5de2fa206006
Dinesh Kumar <gdineshda@gmail.com>
2021-10-19 21:38:48 +0530

    started chapter on functions

---
+++ chapter8/simpleBarFunction.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#define NAME "Gigathink, Inc."
+#define ADDRESS "101 Megabuck Plaza"
+#define PLACE "Megapolis, CA 94040"
+#define WIDTH 40
+
+
+void starbar(int width);
+int main(void) {
+    starbar(WIDTH);
+    printf("%s\n", NAME);
+    printf("%s\n", ADDRESS);
+    printf("%s\n", PLACE);
+    starbar(WIDTH);
+}
+
+void starbar(int width) {
+    for (int i = 0; i < width; i++)
+        putchar('*');
+    putchar('\n');
+}
